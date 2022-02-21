a69dc44fceb12128bf43a7a214ebf8e35213c478
w2.gperez <ierman1@hotmail.com>
2022-02-21 23:32:35 +0100

    Botones menu principal

--- app/src/main/java/com/kronostudios/the_game/MainMenu.java
+++ app/src/main/java/com/kronostudios/the_game/MainMenu.java
@@ -2,29 +2,31 @@ package com.kronostudios.the_game;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Application;
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.Html;
-import android.text.method.ScrollingMovementMethod;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.PopupWindow;
-import android.widget.TextView;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainMenu extends AppCompatActivity {
 
-    Boolean opened;
+    // Menu buttons
+    Button btnPlay;
+    Button btnCollection;
+    Button btnProfile;
+    Button btnExit;
+
     // Legal popup
-    PopupWindow popUp;
+    PopupWindow legalPopup;
+    Boolean legalPopupOpened;
     Button btnShowLegalPopup;
     Button btnCloseLegalPopup;
-    Button termsBtn;
-    Button aboutBtn;
+    Button btnLegal;
+    Button btnAbout;
 
     MainMenu menu;
     Boolean click;
@@ -35,11 +37,45 @@ public class MainMenu extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
 
-        opened = false;
+        legalPopupOpened = false;
         Intent intent = getIntent();
         fromSplash = intent.getStringExtra("fromSplash");
         menu = this;
         click = false;
+
+        btnPlay = (Button) findViewById(R.id.btnPlay);
+        btnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TODO Play button method
+            }
+        });
+
+        btnCollection = (Button) findViewById(R.id.btnCollection);
+        btnCollection.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TODO Collection button method
+            }
+        });
+
+        btnProfile = (Button) findViewById(R.id.btnProfile);
+        btnProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TODO Profile button method
+            }
+        });
+
+        btnExit = (Button) findViewById(R.id.btnExit);
+        btnExit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TODO No se cierra la app (vuelve a la splash screen)
+                finish();
+                System.exit(0);
+            }
+        });
 
         btnShowLegalPopup = (Button) findViewById(R.id.btnShowLegal);
         btnShowLegalPopup.setOnClickListener(new View.OnClickListener() {
@@ -47,8 +83,8 @@ public class MainMenu extends AppCompatActivity {
             public void onClick(View v) {
                 //instantiate the popup.xml layout file
 
-                if(!opened){
-                    opened = true;
+                if(!legalPopupOpened){
+                    legalPopupOpened = true;
 
                     LayoutInflater layoutInflater = (LayoutInflater) MainMenu.this.getSystemService(getApplicationContext().LAYOUT_INFLATER_SERVICE);
                     View customView = layoutInflater.inflate(R.layout.popup_window, null);
@@ -56,22 +92,22 @@ public class MainMenu extends AppCompatActivity {
                     btnCloseLegalPopup = (Button) customView.findViewById(R.id.btnCloseLegal);
 
                     //instantiate popup window
-                    popUp = new PopupWindow(customView, LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+                    legalPopup = new PopupWindow(customView, LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
 
                     //display the popup window
-                    popUp.showAtLocation(findViewById(R.id.mainMenuLayout), Gravity.CENTER, 0, 0);
+                    legalPopup.showAtLocation(findViewById(R.id.mainMenuLayout), Gravity.CENTER, 0, 0);
 
                     //close the popup window on button click
                     btnCloseLegalPopup.setOnClickListener(new View.OnClickListener() {
                         @Override
                         public void onClick(View v) {
-                            opened = false;
-                            popUp.dismiss();
+                            legalPopupOpened = false;
+                            legalPopup.dismiss();
                         }
                     });
 
-                    termsBtn = (Button) customView.findViewById(R.id.btnTerms);
-                    termsBtn.setOnClickListener(new View.OnClickListener() {
+                    btnLegal = (Button) customView.findViewById(R.id.btnTerms);
+                    btnLegal.setOnClickListener(new View.OnClickListener() {
                         @Override
                         public void onClick(View view) {
                             Intent myIntent = new Intent(MainMenu.this, TermsAndCond.class);
@@ -79,8 +115,8 @@ public class MainMenu extends AppCompatActivity {
                         }
                     });
 
-                    aboutBtn = (Button) customView.findViewById(R.id.btnAbout);
-                    aboutBtn.setOnClickListener(new View.OnClickListener() {
+                    btnAbout = (Button) customView.findViewById(R.id.btnAbout);
+                    btnAbout.setOnClickListener(new View.OnClickListener() {
                         @Override
                         public void onClick(View view) {
                             Intent myIntent = new Intent(MainMenu.this, About.class);
@@ -91,9 +127,10 @@ public class MainMenu extends AppCompatActivity {
             }
         });
     }
+
     @Override
     public void onBackPressed() {
-       if(fromSplash.equals("true")){
+       if (fromSplash.equals("true")){
            return;
        }
     }
