29c1f3ec6ab0995dd0bbbfab8ba8ca639760188b
Guillem Caballé <guillemcaballe95@gmail.com>
2022-03-06 00:13:47 +0100

    Les tres tonteries afegides a activity_game_board que vaig fer a clase

--- app/src/main/java/com/kronostudios/the_game/activities/GameBoard.java
+++ app/src/main/java/com/kronostudios/the_game/activities/GameBoard.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.kronostudios.the_game.R;
+import com.kronostudios.the_game.models.Card;
+
+import java.util.List;
 
 public class GameBoard extends AppCompatActivity {
 
@@ -13,4 +16,13 @@ public class GameBoard extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_board);
     }
+
+    public void populateGameBoard(List<Card> hand, List<Character> alliedChars, List<Character> enemyChars){
+
+        for(int i = 0 ; i < alliedChars.size(); i++){
+
+        }
+
+    }
+
 }
